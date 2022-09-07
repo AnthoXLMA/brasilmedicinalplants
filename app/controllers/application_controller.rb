ed6bfcf7b88a569094d2c413522e96eab8a6a866
AnthoXLMA <contact@service-lite-plus.com>
2022-09-07 14:46:35 -0300

    first modeling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :activate_profiler
+  def activate_profiler
+    ENV['RACK_MINI_PROFILER'] = 'on' if params['pp'] =='disabled'
+    ENV['RACK_MINI_PROFILER'] = 'off' if params['pp'] == 'disabled'
+  end
 end
