14595a898418bf16c4771d0aee2fbfde6532b4c9
Anthony Mania <amj2544@tutanota.com>
2024-05-21 18:34:20 +0200

    yarnclosed 2

--- app.js
+++
@@ -1,8 +0,0 @@
-// {
-//   "name": "My plantbook: Ruby",
-//   "description": "A plantbook from brasil",
-//   "image": "heroku/ruby",
-//   "repository": "https://github.com/AnthoXLMA/brasilmedicinalplants",
-//   "keywords": ["ruby", "rails" ],
-//   "addons": ["heroku-postgresql"]
-// }
