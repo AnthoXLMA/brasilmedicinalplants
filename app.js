020cc2257cbf8c9e7e6d23f1d5ec007398e4037d
Anthony Mania <amj2544@tutanota.com>
2024-05-21 15:45:55 +0200

    heroku deploy failed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-{
-  "name": "My plantbook: Ruby",
-  "description": "A plantbook from brasil",
-  "image": "heroku/ruby",
-  "repository": "https://github.com/AnthoXLMA/brasilmedicinalplants",
-  "keywords": ["ruby", "rails" ],
-  "addons": ["heroku-postgresql"]
-}
+// {
+//   "name": "My plantbook: Ruby",
+//   "description": "A plantbook from brasil",
+//   "image": "heroku/ruby",
+//   "repository": "https://github.com/AnthoXLMA/brasilmedicinalplants",
+//   "keywords": ["ruby", "rails" ],
+//   "addons": ["heroku-postgresql"]
+// }
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Brasilmedicinalplants
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.initialize_on_precompile = true
+    # config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
